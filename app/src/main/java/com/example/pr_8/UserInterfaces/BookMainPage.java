c3ec6c8e9617d41086fc3368c7f17eaefb567856
Andrey <mr.dallas007@gmail.com>
2023-05-05 21:51:07 +0300

    Implement ExecutorService multithreading algorithm

--- app/src/main/java/com/example/pr_8/UserInterfaces/BookMainPage.java
+++ app/src/main/java/com/example/pr_8/UserInterfaces/BookMainPage.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,9 +16,19 @@ import androidx.navigation.Navigation;
 
 import com.example.pr_8.R;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class BookMainPage extends Fragment {
 
     private static final String TAG = "BookMainPage";
+
+
 
     public BookMainPage() {
         super(R.layout.fragment_book_main_page);
@@ -38,8 +50,33 @@ public class BookMainPage extends Fragment {
         TextView text2 = view.findViewById(R.id.NookAuthor);
         text2.setText("Автор: " + getArguments().getString("author"));
 
+        List<Future<Integer>> callable = new ArrayList<>();
+        ProgressBar bar = view.findViewById(R.id.progressBar);
+        Callable<Integer> callableTask = () -> {
+        try {
+                for(int i =0; i<=100; i++){
+                    int finalI = i;
+                    bar.post(() -> bar.setProgress(finalI));
+                    Thread.sleep(100);
+                }
+        } catch(InterruptedException e){
+            throw new RuntimeException(e);
+        }
+                return 1;
+        };
+
         Button addButton = view.findViewById(R.id.addButton);
         addButton.setOnClickListener(view1 -> Navigation.findNavController(view1).navigate(R.id.action_blankFragment4_to_libraryFragment));
+
+
+
+        final ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+
+        Button button = view.findViewById(R.id.executorButton);
+        button.setOnClickListener(view1 -> {
+            callable.add(executorService.submit(callableTask));
+        });
     }
 
 }
